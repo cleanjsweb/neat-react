9442924ed8e5281faafacae13d7c2317f93a0b70
Feranmi Akinlade <30551780+uxFeranmi@users.noreply.github.com>
2025-02-24 02:30:42 +0100

    Update index.ts

--- index.ts
+++ index.ts
@@ -4,12 +4,4 @@ export * from '@/base';
 export * from '@/helpers';
 
 
-
-/* 
-withFetchApi(baseUrl); To mimic axios.get and axios.post type calls.
-@cleanweb/mem-store - Release global-store package here.
-Use mem-store to cache requests in withFetchApi(); Use md5 hashed url+body as key.
-@todo Add simple persistence layer with indexed db.
-@cleanweb/subscribable - To publish changes in the data to subscribers.
-@cleanweb/reactive-data - To combine all 4.
-*/
+export type { EmptyObject, NeverObject };
