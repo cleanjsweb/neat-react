8007332650349501c8180ece0d3391c66464167b
Feranmi Akinlade <30551780+uxFeranmi@users.noreply.github.com>
2025-04-16 08:56:26 +0100

    Add StateFragment type

--- helpers/index.ts
+++ helpers/index.ts
@@ -4,6 +4,8 @@
  * @module Helpers
  */
 
+import type { ComponentLogic } from '@/classy';
+
 export * from './mount-state';
 export * from './rerender';
 export * from './use-component';
@@ -18,3 +20,9 @@ export * from './type-guards';
  * It returns (void) without performing any operations.
  */
 export const noOp = () => {};
+
+export type StateFragment<
+	TComponent extends ComponentLogic<any>
+> = Partial<ReturnType<TComponent['getInitialState']>>;
+
+export type { StateFragment as SF };
